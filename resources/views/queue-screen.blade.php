a2a009968b2e3093e98b41e04cf811060e7d994d
iksan <mdiksann@gmail.com>
2025-12-29 14:43:02 +0700

    feat: add initial HTML structure for QueueScreen view

---
+++ resources/views/queue-screen.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Hospital Queue Screen</title>
+    @vite('resources/js/app.jsx')
+</head>
+<body class="bg-gray-100">
+    <div id="app"></div>
+</body>
+</html>
