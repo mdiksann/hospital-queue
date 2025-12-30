f948d2362d244b5c3bc5b17b6711029b04b8bb8c
iksan <mdiksann@gmail.com>
2025-12-30 16:07:51 +0700

    feat: organize route definitions and ensure Inertia usage for Antrian screen

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::view('/queue-screen', 'queue-screen');
+
+use Inertia\Inertia;
+
+Route::get('/screen/antrian', function () {
+    return Inertia::render('Screen/Antrian');
+});
