96268e871421f6916e5f215b079a0e3afd71c489
iksan <mdiksann@gmail.com>
2025-12-30 16:07:47 +0700

    feat: create app.blade.php for Inertia.js integration

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title inertia></title>
+
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+
+    @inertiaHead
+</head>
+<body>
+    @inertia
+</body>
+</html>
