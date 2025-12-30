d6317b2e2ae0bdfa45ca6b7eba159aab8239cabc
iksan <mdiksann@gmail.com>
2025-12-30 16:07:43 +0700

    feat: add Antrian component for displaying queue screen

---
+++ resources/js/pages/Screen/Antrian.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Head } from '@inertiajs/react'
+
+export default function Antrian() {
+    return (
+        <>
+            <Head title="Screen Antrian" />
+            <div style={{
+                minHeight: '100vh',
+                backgroundColor: '#0f172a',
+                color: 'white',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: '48px',
+                fontWeight: 'bold'
+            }}>
+                SCREEN ANTRIAN
+            </div>
+        </>
+    )
+}
