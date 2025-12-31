c079f41d76d39b995b6cc9cfec681098bb414853
iksan <mdiksann@gmail.com>
2025-12-31 15:55:00 +0700

    feat: remove Echo and Pusher integration from bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,16 +1,7 @@
-import Echo from 'laravel-echo'
-import Pusher from 'pusher-js'
+import axios from 'axios'
 
-window.Pusher = Pusher
+window.axios = axios
 
-window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    wssPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: false,
-    encrypted: false,
-    disableStats: true,
-    enabledTransports: ['ws', 'wss'],
-})
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+import './echo'
