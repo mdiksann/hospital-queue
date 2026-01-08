b9e7c8a3c1c107c9b40f197ecc1d69e184dd3e78
iksan <mdiksann@gmail.com>
2026-01-08 13:12:45 +0700

    style: format app.jsx for consistency and readability

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,18 +1,19 @@
-import './bootstrap'
-import React from 'react'
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+import "./bootstrap";
+import "../css/app.css";
+import React from "react";
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
 
 createInertiaApp({
     resolve: (name) => {
-        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-        return pages[`./Pages/${name}.jsx`]
+        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+        return pages[`./Pages/${name}.jsx`];
     },
     setup({ el, App, props }) {
         createRoot(el).render(
             <React.StrictMode>
                 <App {...props} />
             </React.StrictMode>
-        )
+        );
     },
-})
+});
